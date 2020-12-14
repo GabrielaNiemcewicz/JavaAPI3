8b8ef07355c45df239276479a3901a7222100197
GabrielaNiemcewicz <gabriela.niemcewicz@ucdconnect.ie>
2020-12-14 14:49:29 +0100

    Interface for tests, methods of largest scope

---
+++ Color.java
@@ -0,0 +1,9 @@
+
+public class Color {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ColorAPI.java
@@ -0,0 +1,10 @@
+
+public interface ColorAPI {
+	public int getColor1(); 
+	public int getColor2();
+	public int getColor3();
+	public String getModel();
+	public boolean isEqual(Color color);
+	public void add(Color color);
+
+}
