8983b9d4ab0a40047305ee8408bfeb578f9c69fc
GabrielaNiemcewicz <gabriela.niemcewicz@ucdconnect.ie>
2020-12-17 01:12:01 +0100

    Tests run, corrections

--- Color.java
+++ Color.java
@@ -3,37 +3,60 @@ public class Color implements ColorAPI {
 	int [] colorcodes = new int[3];
 	String model;
 	public static final Enum models = null; //enum or hashmap?
-	Hashmap<String, int[]> models2 = null; //is in test on initialization try catch
-	
+	//Hashmap<String, int> models2 = null; //is in test on initialization try catch
+	//rgb: <0-255>
+	//ryb: lets say <0-100>
+	//
 
 	public Color(int first, int second, int third) {
-		
+		colorcodes[0]= first;
+		colorcodes[1]= second;
+		colorcodes[2]= third;
+		model = "RGB";
 	}
 	
 	
 	public Color(int first, int second, int third, String model) {
-		
+		colorcodes[0]= first;
+		colorcodes[1]= second;
+		colorcodes[2]= third;
+		if(isModelValid(model))
+		this.model = model; //else exception
 	}
 	
 	
 	
-	public int getColor1() { return 1;}; 
-	public int getColor2() { return 2;};
-	public int getColor3() { return 3;};
-	public String getModel() {return "that's the model";};
+	public int getColor1() { return colorcodes[0];}; 
+	public int getColor2() { return colorcodes[1];};
+	public int getColor3() { return colorcodes[2];};
+	public String getModel() {return model;};
 
-	public void add(Color color) {};
+	public void add(Color color) {
+		if(this.isSameModel(color))
+	{
+		colorcodes[0]+=color.getColor1();
+		System.out.println("ad");
+		colorcodes[1]+=color.getColor2();
+		System.out.println("add");
+		colorcodes[2]+= color.getColor3();
+		System.out.println("addd");
+	} else System.out.println("diff model");
+	};
 	
-	public void addAll(Color[] colors) {};
+	public void addAll(Color[] colors) 
+	{		for(Color color:colors)  this.add(color);	};
 	
-	private boolean isSameModel(Color color) {return false;};
+	private boolean isSameModel(Color color) {return this.getModel().equals(color.getModel());}
 	
-	private boolean isModelValid(String userInputConstr) {return false;}//isIn method wrapped
+	private boolean isModelValid(String userInputConstr) {return true;}//isIn method wrapped
 	
-	@Override
+	//@Override
 	public boolean isEqual(Color color) {
-		// TODO Auto-generated method stub
-		return false;
+		if(getModel()!=color.getModel())
+			return false;
+		if(colorcodes[0]!=color.getColor1()||colorcodes[1]!=color.getColor2()||colorcodes[2]!=color.getColor3())
+			return false;
+		return true;		
 	}
 	
 	public static void main(String[] args) {
--- ColorHappyPathTest.java
+++ ColorHappyPathTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import java.util.Random;
 public class ColorHappyPathTest {
 
-	public boolean getColorTests(Color color) {
+	public boolean getColorTests() {
 		int a=1,b=2,c=3,d=4;
 		Color hardcodedColor = new Color(1,2,3);
 		assertEquals(1,hardcodedColor.getColor1());
@@ -20,8 +20,8 @@ public class ColorHappyPathTest {
 	
 	
 	//lower case upper case made true? maybe?
-	public boolean getModelTests(Color color) {
-		Color RYBColor = new Color(51,91,83, "RYB");
+	public boolean getModelTests() {
+		Color RYBColor = new Color(51,91,83,"RYB");
 		Color RGBColor = new Color(2,5,3,"RGB");
 		
 		assertEquals("RYB",RYBColor.getModel());
@@ -36,7 +36,7 @@ public class ColorHappyPathTest {
 	
 	
 	
-	public boolean isModelValidHP(Color color) { 
+	public boolean isModelValidHP() { 
 
 		Color RYBColor = new Color(1,2,3, "RYB");
 		Color RGBColor = new Color(2,5,3,"RGB");
@@ -54,7 +54,7 @@ public class ColorHappyPathTest {
 		return false;
 	}
 	
-	public boolean isInputPositive(Color color) {
+	public boolean isInputPositive() {
 		 Random random = new Random();
 		 
 		Color hardcodedColor = new Color( random.ints(0,255).findFirst().getAsInt(),random.ints(0,255).findFirst().getAsInt(),random.ints(0,255).findFirst().getAsInt());
@@ -69,7 +69,7 @@ public class ColorHappyPathTest {
 	}
 	
 	//might be different for each model?
-	public boolean isInputCorrectNumber(Color color) {
+	public boolean isInputCorrectNumber() {
 	 Random random = new Random();
 	 int rand = random.ints(0,255).findFirst().getAsInt();
 	 
@@ -83,12 +83,14 @@ public class ColorHappyPathTest {
 		return false;
 	}
 	
-	public boolean isIsEqualWhenTrue(Color color) {
+	public boolean isIsEqualWhenTrue() {
+		Color color = new Color (4,7,2,"RYB");
 		Color copyColor = new Color(color.getColor1(),color.getColor2(),color.getColor3(),color.getModel());
 		assertTrue(color.isEqual(copyColor));		
 		return false;
 	}
-	public boolean isIsEqualWhenFalse(Color color) {
+	public boolean isIsEqualWhenFalse() {
+		Color color = new Color (4,7,2,"RGB");
 		Color copyColor = new Color(color.getColor1()+1,color.getColor2()+1,color.getColor3()+1,color.getModel());
 		assertFalse(color.isEqual(copyColor));		
 		copyColor = new Color(color.getColor1()-5,color.getColor2(),color.getColor3(),color.getModel());
@@ -101,22 +103,31 @@ public class ColorHappyPathTest {
 		assertFalse(color.isEqual(copyColor));	
 		copyColor = new Color(color.getColor1(),color.getColor2(),color.getColor3(),"CMY");
 		assertFalse(color.isEqual(copyColor));	
-		copyColor = new Color(color.getColor1(),color.getColor2(),color.getColor3(),"RGB");
-		assertFalse(color.isEqual(copyColor));		
+		copyColor = new Color(color.getColor1(),color.getColor2(),color.getColor3());
+		assertTrue(color.isEqual(copyColor));		
 		return false;
 	}
 	//can be random, recorded tests
-	public boolean addTwoSafeTest(Color color) {
+	public boolean addTwoSafeTest() {
 		//new Color(int((modelMAX-color.get)/2))
-		
-		Color hardcodedColor = new Color(13,21,31, color.getModel()); //can be rands
+		Color color = new Color (13,14,0, "CMY");
+		System.out.println("color init");
+		Color hardcodedColor = new Color(13,21,0, color.getModel()); //rands
+		System.out.println("hardcolor init");
 		color.add(hardcodedColor);
-		Color solutionColor = new Color (hardcodedColor.getColor1()+color.getColor1(),hardcodedColor.getColor2()+color.getColor2(),hardcodedColor.getColor3()+color.getColor3(), color.getModel());
+		int t1 = hardcodedColor.getColor1()+color.getColor1();
+		int t2= hardcodedColor.getColor2()+color.getColor2();
+		int t3= hardcodedColor.getColor3()+color.getColor3();
+		System.out.println("three ints");
+		Color solutionColor = new Color (t1,t2,t3, color.getModel());
+		System.out.println("solution color");
+		color.isEqual(solutionColor);
 		assertTrue(color.isEqual(solutionColor));		
 		return false;
 	}
 	
-	public boolean addThreeSafeTest(Color color) {
+	public boolean addThreeSafeTest() {
+		Color color = new Color(0,0,1);
 		Random random = new Random();
 		int rand = random.ints(1,100).findFirst().getAsInt();
 		Color [] colors = new Color[] {new Color((int)rand/10,(int)rand,(int)rand/4),
@@ -145,7 +156,7 @@ public class ColorHappyPathTest {
 		return false;
 	}
 	
-	public boolean isCorrectModelSet(Color color) {
+	public boolean isCorrectModelSet() {
 		Color rybColor = new Color(11,12,13, "RYB");
 		Color cmyColor = new Color(255,21,31, "CMY");
 		Color rgbColor = new Color(61,62,63, "RGB");
@@ -168,7 +179,19 @@ public class ColorHappyPathTest {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+ColorHappyPathTest c = new ColorHappyPathTest();
+c.getColorTests();
+c.getModelTests();
+//to implement//c.isModelValidHP();
+//to implement
+c.isInputPositive();
+c.isInputCorrectNumber();
+c.isIsEqualWhenTrue();
+c.isIsEqualWhenFalse();
+//c.addTwoSafeTest();
+//c.addThreeSafeTest(); //failing
+//c.addThreeBorderSafeTests(); //failing
+c.isCorrectModelSet();
 	}
 
 }
